e0b53731214788d2d5290608ea102b549cbb4b85
TonyStinson1 <pankajswami69@gmail.com>
2023-02-09 21:17:13 +0530

    Workflow changes for the lists and header

--- ios/Refrensrick/AppDelegate.mm
+++ ios/Refrensrick/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"rickmorty", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Refrensrick", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
